f886983b7fdf9592ee6d7ed3880beac144642c27
zach-he <971499086@qq.com>
2020-08-27 22:14:08 +0800

    [rank][zach]feature:add test case2 : test voyageRisk length=8 && zone=china

--- test/rankTest.js
+++ test/rankTest.js
@@ -8,3 +8,11 @@ rankTest('case1 : test voyageRisk length=4 && zone=china', t => {
 }
   t.is(voyageRisk(voyage), 5)
 })
+
+rankTest('case2 : test voyageRisk length=8 && zone=china', t => {
+  const voyage = {
+    length: 8,
+    zone: 'china'
+}
+  t.is(voyageRisk(voyage), 7)
+})
