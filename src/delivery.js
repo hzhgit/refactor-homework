19d029b787630eb143e30d1c7cecb42566565e09
zach-he <971499086@qq.com>
2020-08-27 21:18:16 +0800

    [zach]refactor:extract isRushTrue method

--- src/delivery.js
+++ src/delivery.js
@@ -1,22 +1,26 @@
+function isRushTrue(anOrder){
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  }
+  else if ([
+    'NY',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else {
+    deliveryTime = 3;
+  }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    return isRushTrue(anOrder)
   }
   else {
     let deliveryTime;
