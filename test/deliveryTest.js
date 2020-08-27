53b42a149ad55452d924f54393b678d0b924169f
zach-he <971499086@qq.com>
2020-08-27 21:01:43 +0800

    [zach]feature:add test case 3:isRush is false && deliveryState is MA

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -26,3 +26,16 @@ deliveryTest('case 2:isRush is ture && deliveryState is NY', t => {
 
     t.is(3, deliveryDate(anOrder, true))
 })
+
+deliveryTest('case 3:isRush is false && deliveryState is MA', t => {
+    const anOrder = {
+        deliveryState: 'MA',
+        placedOn: {
+            plusDays: (deliveryTime) => {
+                return deliveryTime;
+            }
+        }
+    }
+
+    t.is(4, deliveryDate(anOrder, false))
+})
