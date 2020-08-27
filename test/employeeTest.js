d4e05a02870bb3d27eb3c55de869ee0c25f6e0a6
zach-he <971499086@qq.com>
2020-08-27 21:49:13 +0800

    [employee][zach]:add test case 2:test validateType

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -5,3 +5,11 @@ employeeTest('case 1:test toString',t => {
     const employee =new Employee("Zach","engineer");
     t.is("Zach (engineer)",employee.toString())
 })
+
+employeeTest('case 2:test validateType',t => {
+    try {
+        const employee =new Employee("Zach","boss");
+    } catch (error) {
+        t.is('Employee cannot be of type boss',error.message)
+    }
+})
