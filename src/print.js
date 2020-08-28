9636bad1a68dbee2e90a63a8580b742edb07a634
hzhgit <971499086@qq.com>
2020-08-28 11:34:05 +0800

    [print][zach]refactor:modify console.log to return string

--- src/print.js
+++ src/print.js
@@ -1,8 +1,8 @@
 function printOwing (invoice) {
   let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+  let result = '***********************\n'
+    +'**** Customer Owes ****\n'
+    +'***********************\n';
 
   // calculate outstanding
   for (const o of invoice.borderSpacing) {
@@ -14,9 +14,9 @@ function printOwing (invoice) {
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
   // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+  return result += `name: ${invoice.customer}\n`
+    +`amount: ${outstanding}\n`
+    +`amount: ${invoice.dueDate.toLocaleDateString()}`
 }
 
 module.exports = { printOwing }
