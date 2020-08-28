cab6c4ca84c96178223205f52ac7c25779dab232
zach-he <971499086@qq.com>
2020-08-28 21:04:28 +0800

    [rank][zach]feature:add test voyageRisk case 4 : given voyage length=8 && zone=china

--- test/rankTest.js
+++ test/rankTest.js
@@ -19,11 +19,20 @@ rankTest('voyageRisk case 2 : given voyage length=3 && zone=china', t => {
   t.is(result, 5);
 });
 
-rankTest('voyageRisk case 3 : given voyage length=5 && zone=USA', t => {
+rankTest('voyageRisk case 3 : given voyage length=8 && zone=USA', t => {
   const voyage = {
-    length: 3,
+    length: 8,
     zone: 'USA'
   };
   const result = voyageRisk(voyage);
-  t.is(result, 1);
+  t.is(result, 3);
+});
+
+rankTest('voyageRisk case 4 : given voyage length=8 && zone=china', t => {
+  const voyage = {
+    length: 8,
+    zone: 'china'
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 7);
 });
