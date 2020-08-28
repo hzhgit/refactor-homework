3f6610778c409abf8d5691755f3afa9e0f3dbb41
zach-he <971499086@qq.com>
2020-08-28 20:57:52 +0800

    [rank][zach]feature:add test voyageRisk case 1 : given voyage length=3 && zone=USA

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,3 +1,11 @@
 const rankTest = require('ava');
 const { voyageRisk, rating, captainHistoryRisk, voyageProfitFactor } = require("../src/rank");
 
+rankTest('voyageRisk case 1 : given voyage length=3 && zone=USA', t => {
+  const voyage = {
+    length: 3,
+    zone: 'USA'
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 1);
+});
